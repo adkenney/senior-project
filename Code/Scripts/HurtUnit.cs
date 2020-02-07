e2c62a9a823a53173feabd84d62ee6e400723e78
adkenney <adkenney@aggies.ncat.edu>
2020-02-07 17:28:30 -0500

    Add health and stats scripts

---
+++ Code/Scripts/HurtUnit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtUnit : MonoBehaviour
+{
+    public int attackDamageToGive;
+    private int currentDamage;
+
+    private UnitStats unitStat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unitStat = FindObjectOfType<UnitStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /* hurt opposing unit when attacked */
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //replace "" with a player game object name
+        if (other.gameObject.name == "") {
+            currentDamage = attackDamageToGive - unitStat.unitDefense;
+            other.gameObject.GetComponent<HealthManager>().HurtUnit(currentDamage);
+        } 
+    }
+}
---
+++ Code/Scripts/UnitStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public int unitMaxEnergy;
+    public int unitCurrentEnergy;
+    public int unitAttack;
+    public int unitDefense;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unitCurrentEnergy = unitMaxEnergy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
